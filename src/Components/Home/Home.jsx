b4c09c30fecd1d27f66db457965bcadf4229fbc5
MacDuki <PCMYACT@hotmail.com>
2023-12-02 23:46:01 -0300

    ...

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,57 @@
+import { motion } from "framer-motion";
 import bStyle from "./background.module.css";
 function Home() {
 	return (
 		<section
 			className={`flex flex-col items-center justify-center  ${bStyle.background}`}>
 			<div className='w-full relative z-10 mb-20'>
-				<div className='absolute inset-y-36 inset-x-1/3  flex flex-col items-center font-tittle font-bold text-2xl text-bgWaveLight md:text-bgLight md:inset-y-32 md:text-3xl lg:inset-y-72 lg:text-5xl lg:text-bgLight'>
-					<h1 className='w-auto whitespace-nowrap '>Pedro Wattimo</h1>
-					<h2 className='w-auto whitespace-nowrap'>FullStack Developer</h2>
+				<div className='absolute inset-y-36 inset-x-1/3  flex flex-col items-center font-tittle cursor-default font-bold text-2xl text-bgWaveLight md:text-bgLight md:inset-y-32 md:text-3xl lg:inset-y-72 lg:text-5xl lg:text-bgLight'>
+					<motion.h1
+						animate={{ y: 0 }}
+						initial={{ y: 500 }}
+						transition={{
+							type: "spring",
+							stiffness: 363,
+							damping: 80,
+							mass: 4,
+						}}
+						className='w-auto whitespace-nowrap '>
+						Pedro Wattimo
+					</motion.h1>
+					<motion.h2
+						animate={{ y: 0 }}
+						initial={{ y: -500 }}
+						transition={{
+							type: "spring",
+							stiffness: 363,
+							damping: 80,
+							mass: 4,
+						}}
+						className='w-auto whitespace-nowrap'>
+						FullStack Developer
+					</motion.h2>
 				</div>
-				<img
+				<motion.img
+					animate={{ y: 0 }}
+					initial={{ y: 500 }}
+					transition={{
+						type: "spring",
+						stiffness: 363,
+						damping: 80,
+						mass: 4,
+					}}
 					className='w-full h-full md:h-auto lg:h-auto'
 					src='src\assets\waveH2.svg'
 				/>
-				<img
+				<motion.img
+					animate={{ y: 0 }}
+					initial={{ y: -500 }}
+					transition={{
+						type: "spring",
+						stiffness: 363,
+						damping: 80,
+						mass: 4,
+					}}
 					className='w-full h-full md:h-auto lg:h-auto'
 					src='src\assets\waveH1.svg'
 				/>
